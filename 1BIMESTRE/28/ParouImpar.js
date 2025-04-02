710d4f5ad911969e7f04c29cd24827525b3f33c2
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2025-04-02 19:13:59 -0300

    .

---
+++ 1BIMESTRE/28/ParouImpar.js
@@ -0,0 +1,13 @@
+let num = 5;
+let resultado = "";
+
+if(num % 2 == 0) {
+    console.log("Par");
+}else{
+    console.log("Ímpar");
+}
+
+/*console.log((num % 2 == 0) ? "Par":"Ímpar");*/
+
+resultado = ((num % 2 == 0) ? "Par":"Ímpar");
+console.log(resultado)
